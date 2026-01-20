fae7f6c74ae4da3392b5b9867d34645d36b2bf79
Skill-World <skillgamedeveloper@hotmail.com>
2026-01-20 19:33:17 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-
+import ProductDetail from './pages/ProductDetail';
 import React from 'react';
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
